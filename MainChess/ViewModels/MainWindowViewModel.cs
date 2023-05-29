f89feda69f3e16765e364e9fe12a293c1462abd5
Kargin Maxim <karginmaxx@gmail.com>
2023-05-29 21:27:37 +0300

    класс модели представления главного окна

---
+++ MainChess/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MainChess.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainChess.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _title = "NoteApp";
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+    }
+}
